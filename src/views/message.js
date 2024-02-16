cbd459f0e96cc64e1d08f5b63a0ec8e35c1715a4
Richard-Cambe <richard.cambe@outlook.fr>
2024-02-16 14:27:13 +0100

    CSS Better

--- src/views/message.js
+++ src/views/message.js
@@ -3,13 +3,13 @@ export default () => (`
     <div class="col-6">
     </div>
     <div class="col-6">
-      <div class="card mb-3" style="max-width: 540px;">
+      <div class="MyText" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+              <h5 class="Mycard-title">Card title</h5>
+              <p class="Mycard-text">My Text</p>
+              <p class="Mycard-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
           <div class="col-md-4">
@@ -22,12 +22,12 @@ export default () => (`
 
   <div class="row">
     <div class="col-6">
-      <div class="card mb-3" style="max-width: 540px;">
+      <div class="botText" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">Bot Text</p>
               <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
