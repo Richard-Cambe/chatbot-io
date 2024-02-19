c18a9f6748e1c608afe66f1cabf816e59b3e8eac
Richard-Cambe <richard.cambe@outlook.fr>
2024-02-19 15:03:50 +0100

    hey

--- src/views/contacts.js
+++ src/views/contacts.js
@@ -10,28 +10,28 @@ export default () => (`
         <img src="${bot}" class="rounded-circle" width="70" alt="...">
         <h6>Bot 4</h6>
       </div>
-      <span class="badge bg-primary rounded-pill">140</span>
+      <span class="badge rounded-pill">140</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <div class="d-flex align-items-center">
         <img src="${greenBot}" class="rounded-circle" width="70" alt="...">
         <h6>Bot 1</h6>
       </div>
-      <span class="badge bg-primary rounded-pill">1</span>
+      <span class="badge rounded-pill">1</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <div class="d-flex align-items-center">
         <img src="${brownBot}" class="rounded-circle" width="70" alt="...">
         <h6>Bot 2</h6>
       </div>
-      <span class="badge bg-primary rounded-pill">15</span>
+      <span class="badge rounded-pill">15</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <div class="d-flex align-items-center">
         <img src="${yellowBot}" class="rounded-circle" width="70" alt="...">
         <h6>Bot 3</h6>
       </div>
-      <span class="badge bg-primary rounded-pill">7</span>
+      <span class="badge rounded-pill">7</span>
     </li>
   </ul>
 `);
--- src/views/nav.js
+++ src/views/nav.js
@@ -5,15 +5,15 @@ import viewContacts from './contacts';
 export default () => (`
 <div class="Nav">
   <img src="${logo}" class="fox" alt="...">
-  <a href="/home"><h1>Chatbot-io</h1></a>
+  <a href="/"><h1>Chatbot-io</h1></a>
   <div class="d-flex align-items-center">
     <img src="${logo}" class="rounded-circle" width="70" alt="...">
     <img src="${greenBot}" class="rounded-circle" width="70" alt="...">
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="fa-solid fa-bars"></i></button>
+    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="fa-solid fa-bars"></i></button>
 
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasRightLabel">BotsList</h5>
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">Bots List</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
