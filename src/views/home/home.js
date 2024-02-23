fed09615992eeb1182521ed2348459b52a6810e9
Richard-Cambe <richard.cambe@outlook.fr>
2024-02-23 14:10:20 +0100

    homepage visuals

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,9 +1,10 @@
 import homefox from './img/fox.png';
+import './index.scss';
 
 export default () => (`
   <div class="home_title">
-    <h1 class="home_h1">Chatbot-io</h1></div>
-    <img src='${homefox}'/>
-    <a href="/message"><button class="hello" type ="button">hello</button></a>
+    <h1 class="home_h1">Chatbot-io</h1>
+    <img class="logo" src='${homefox}'/>
+    <a href="/chatbot"><button class="hellobutton" type ="button">Start chatting</button></a>
   </div>
 `);
